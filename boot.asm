b8a19a4a85d20c8f83e3b0c5b6db89a136eda1d5
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-03-08 19:10:48 +0000

    Improve comments here and there

--- boot.asm
+++ boot.asm
@@ -1,23 +1,30 @@
 bits 16     ; We're dealing with 16 bit code
 org 0x7C00  ; Inform the assembler of the starting location for this code
             ; This is just to generate the right addresses.
-stackBase   equ 0x800 ; 0x7C0 + 0x40 (1024/16 stack size)
-stackSize   equ 0x40  ; 0x40 (1024/16 stack size)
+
+stackSize   equ 0x40                ; 0x40 (1024/16 stack size)
+stackBase   equ 0x7E0 + stackSize   ; 0x7E0 * 16 is the first available byte after the code
+
 
 main:
+    ; Make sure the stack is ready, just in case we need it...
     push stackBase
     pop ss
     mov sp, stackSize
     mov bp, sp
 
+    ; Change video mode to MCGA 320x300, 256 indexed colors
     mov ax, 0x13
     int 10h
 
+    ; Draw a pixel on screen because we can - this will likely go away to save space...
     mov ax, 0xA000
     mov es, ax
     mov bx, 10
     mov al, 7
     mov es:[bx], al
+
+    ; Run through all the colors several time and clear the screen each time
     mov cx, 4096*4
 .loopsydo:
     dec cx
@@ -30,14 +37,16 @@ main:
     jnz .wasteTime
     jmp .loopsydo
 
+    ; ... and curtains.
 .loop:
     jmp .loop
     hlt
 
+    ; Needs the color on the stack
 clearScreen:
     push bp
     mov bp, sp
-    ; bx is going to be the counter
+    ; bx is going to be the counter here
     mov bx, 0xFA00
     mov ax, [bp + 4]
 clearScreen_loop:
