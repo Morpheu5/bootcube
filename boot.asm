459d6342c677178bec4dac5d8fcc489c4b450ca5
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-05-02 15:53:02 +0100

    Add untested matrix multiplication code and code for loading the next sector because we ran out of space

--- boot.asm
+++ boot.asm
@@ -6,7 +6,7 @@ org 0x7C00  ; Inform the assembler of the starting location for this code
     jmp 0:main ; And this is to make sure CS is in the right place.
 
 stackSize   equ 0x40                ; 0x40 (1024/16 stack size)
-stackBase   equ 0x7E0 + stackSize   ; 0x7E0 * 16 is the first available byte after the code
+stackBase   equ 0x800 + stackSize   ; 0x800 * 16 is a good location for the stack
 
 main:
     ; Make sure the stack is ready, just in case we need it...
@@ -15,6 +15,18 @@ main:
     mov sp, stackSize
     mov bp, sp
 
+    ; Load an extra sector (dl contains the drive number)
+    xor ah, ah  ; Reset floppy controller
+    int 0x13
+
+    mov ax, 0x201 ; Load sector 2 from the boot drive
+    mov cx, 0x2   ; Somehow, the sectors start counting from 1, so the second one is number 2...
+    xor dh, dh
+    xor bx, bx
+    mov es, bx
+    mov bx, 0x7e00
+    int 0x13
+
     ; Change video mode to MCGA 320x300, 256 indexed colors
     mov ax, 0x13
     int 10h
@@ -22,24 +34,11 @@ main:
     ; Prepare the video segment
     mov ax, 0xA000
     mov es, ax
-    
+
     push 54
     call clearScreen
     add sp, 2
 
-    push word 200
-    push word 320
-    push word 0
-    push word 0
-    call plotLine
-    add sp, 8
-    push word 200
-    push word 0
-    push word 0
-    push word 320
-    call plotLine
-    add sp, 8
-    
     push 100 ; Make sure these are the right size
     push 160
     call coords ; Returns into bx
@@ -48,11 +47,69 @@ main:
     mov al, 15 ; Sets color to white
     drawPixel ; Draws a white pixel to the given ccordinates
 
-
 ; ... and curtains.
 exit:
     jmp exit
     hlt
+
+matMul:
+        push    ebp
+        mov     ebp, esp
+        xor     ecx, ecx
+        push    edi
+        push    esi
+        xor     esi, esi
+        push    ebx
+        sub     esp, 16
+        movsx   ebx, byte [ebp+20]
+        mov     dword [ebp-20], ecx
+        lea     eax, [0+ebx*4]
+        mov     dword [ebp-28], eax
+    .L2:
+        mov     edx, dword [ebp-20]
+        mov     eax, dword [ebp+16]
+        mov     edi, dword [ebp+8]
+        lea     eax, [eax+edx*4]
+        lea     edx, [edi+esi*4]
+        mov     dword [ebp-24], edx
+        xor     edx, edx
+        mov     dword [ebp-16], edx
+    .L6:
+        cmp     dword [ebp-16], ebx
+        jnb     .L10
+        mov     edi, dword [ebp-16]
+        mov     ecx, dword [ebp+12]
+        mov     dword [eax], 0x00000000
+        lea     ecx, [ecx+edi*4]
+        mov     edi, ecx
+        xor     ecx, ecx
+    .L3:
+        mov     edx, dword [ebp-24]
+        fld     dword [edx+ecx*4]
+        fmul    dword [edi]
+        mov     edx, edi
+        inc     ecx
+        fadd    dword [eax]
+        mov     edi, dword [ebp-28]
+        add     edx, edi
+        mov     edi, edx
+        fstp    dword [eax]
+        cmp     ecx, 4
+        jne     .L3
+        inc     dword [ebp-16]
+        add     eax, 4
+        jmp     .L6
+    .L10:
+        add     esi, 4
+        add     dword [ebp-20], ebx
+        cmp     esi, 16
+        jne     .L2
+        add     esp, 16
+        pop     ebx
+        pop     esi
+        pop     edi
+        pop     ebp
+        ret
 
 plotLine:
     push bp
@@ -164,7 +221,7 @@ coords: ; Returning into bx goes against convention but it makes for more compac
     leave
     ret
 
-    ; Needs the color on the stack
+; Needs the color on the stack
 clearScreen:
     push bp
     mov bp, sp
@@ -182,7 +239,21 @@ clearScreen:
 
 
 
-; times 510-($-$$) db 0 ; Add enough padding to make 510 bytes in total
-; db 0x55, 0xAA ; Boot magic number 0xAA55
 
-;;; 299 bytes
+times 510-($-$$) db 0 ; Add enough padding to make 510 bytes in total
+db 0x55, 0xAA ; Boot magic number 0xAA55
+
+rotMat:
+    dd  0.5  , -0.707,  0.0  ,  0.0
+    dd  0.707,  0.5  ,  0.0  ,  0.0
+    dd  0.0  ,  0.0  ,  1.0  ,  0.0
+    dd  0.0  ,  0.0  ,  0.0  ,  1.0
+
+line:
+    dd  0.0  ,  0.0  ,  0.0  ,  1.0
+    dd  1.0  ,  0.0  ,  0.0  ,  1.0
+
+lineToDraw:
+    times 4 dd 0
+
+times 1024-($-$$) db 0 ; Add enough padding to make 1024 bytes in total
